5d8de623c774eeefbcaebecc9a67492b6344b2b5
zhoukai <18516528912@163.com>
2019-09-10 22:50:05 +0800

    u

--- src/main/java/cn/sh/test0909/Demo090908.java
+++ src/main/java/cn/sh/test0909/Demo090908.java
@@ -12,14 +12,30 @@ public class Demo090908 {
      */
     public static void main(String[] args) {
 
+        TreeNode node1 = new TreeNode(-10);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = null;
+        TreeNode node5 = null;
+        TreeNode node6 = new TreeNode(15);
+        TreeNode node7 = new TreeNode(7);
+        node1.left = node2;
+        node1.right=node3;
+        node3.left=node6;
+        node3.right = node7;
+        int i = maxPathSum(node1);
+        System.out.println(i);
 
     }
 
 
+    /**
+     * 定义最大值得和
+     */
     static int maxSum = Integer.MIN_VALUE;
 
     public static int maxPathSum(TreeNode root) {
-        maxSum = maxPath(root);
+        maxPath(root);
         return maxSum;
     }
 
@@ -29,6 +45,7 @@ public class Demo090908 {
         }
         int left = maxPath(root.left);
         int right = maxPath(root.right);
+        // 计算最大值得和，与上一计算值比较，取最大
         maxSum = Math.max(left + right + root.val, maxSum);
         int temp = Math.max(left, right) + root.val;
         return temp > 0 ? temp : 0;
@@ -39,10 +56,19 @@ public class Demo090908 {
      */
     public static class TreeNode {
 
+        /**
+         * 当前节点值
+         */
         int val;
 
+        /***
+         * 左节点
+         */
         TreeNode left;
 
+        /***
+         * 右节点
+         */
         TreeNode right;
 
         public TreeNode(int val) {
