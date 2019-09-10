20cd35b36d1077236f37e3c82d7319cf088159c0
zhoukai <zhou.kai@puscene.com>
2019-09-10 10:29:22 +0800

    u

---
+++ src/main/java/cn/mwee/demo/Demo1.java
@@ -0,0 +1,12 @@
+package cn.mwee.demo;
+
+/**
+ * @author zhoukai
+ * @date 2019/9/10
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/cn/mwee/test/Test1.java
@@ -0,0 +1,12 @@
+package cn.mwee.test;
+
+/**
+ * @author zhoukai
+ * @date 2019/9/10
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+
+    }
+}
