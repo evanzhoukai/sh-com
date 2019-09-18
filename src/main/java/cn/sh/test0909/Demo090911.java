8be69ce47cb0280ae175e6a7a87e19dc9452ea39
zhoukai <18516528912@163.com>
2019-09-18 20:32:35 +0800

    u

---
+++ src/main/java/cn/sh/test0909/Demo090911.java
@@ -0,0 +1,19 @@
+package cn.sh.test0909;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author zhoukai
+ * @Description:
+ * @date 2019-09-11 21:17
+ */
+public class Demo090911 {
+
+    public static void main(String[] args) {
+
+        Map<String,String> map = new ConcurrentHashMap<>(8);
+
+    }
+}
