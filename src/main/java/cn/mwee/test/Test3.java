a3b08d1c867822103d591c022e9cb545c956c3a8
zhoukai <zhou.kai@puscene.com>
2019-09-19 09:44:48 +0800

    u

--- src/main/java/cn/mwee/test/Test3.java
+++ src/main/java/cn/mwee/test/Test3.java
@@ -1,30 +1,28 @@
 package cn.mwee.test;
 
-import org.apache.activemq.util.ThreadTracker;
-
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhoukai
  * @Description:
- * @date 2019-09-18 21:20
+ * @date 2019-09-18
  */
 public class Test3 {
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new Daemon(),"Deamon");
+        Thread thread = new Thread(new Daemon(), "Deamon");
         thread.setDaemon(true);
         thread.start();
     }
 
-    static class Daemon implements Runnable{
+    static class Daemon implements Runnable {
         @Override
         public void run() {
             try {
                 TimeUnit.SECONDS.sleep(10);
-            }catch (Exception e){
+            } catch (Exception e) {
 
-            }finally {
+            } finally {
                 System.out.println("Daemon Thread finally run.");
             }
         }
