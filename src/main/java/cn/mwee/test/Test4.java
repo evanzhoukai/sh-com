28db3ef9cf7c7d610dd145a3a275e1dcfcc4fa41
zhoukai <zhou.kai@puscene.com>
2019-09-19 10:39:43 +0800

    u

---
+++ src/main/java/cn/mwee/test/Test4.java
@@ -0,0 +1,58 @@
+package cn.mwee.test;
+
+/**
+ * @author zhoukai
+ * @date 2019/9/19
+ */
+public class Test4 {
+    private static final long count = 100001;
+
+    public static void main(String[] args) throws Exception {
+        concurrent();
+        serial();
+    }
+
+    /**
+     * 多线程
+     *
+     * @throws Exception
+     */
+    private static void concurrent() throws Exception {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long end = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("【concurrent times 】" + end + " ms,b = " + b);
+    }
+
+    /**
+     * 单线程
+     */
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("【serial times 】" + time + " ms ,b =  " + b);
+    }
+
+}
